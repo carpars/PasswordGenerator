c312d6768b485b307258d6ada83b798c6495a31f
carpars <carpars@outlook.es>
2020-11-26 19:58:13 +0100

    Code behind - OnPost()   -not working

---
+++ WPA/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WPA.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            throw new Exception("Entra on get");
+        }
+
+        public void Page_Load()
+        {
+            throw new Exception("Entra LOad");
+        }
+
+    }
+}
