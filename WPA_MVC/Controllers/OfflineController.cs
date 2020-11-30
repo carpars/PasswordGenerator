23530378f94a73d0215d1030420160bdb2349efa
Carlos Parra <carpars@outlook.es>
2020-11-30 15:22:59 +0100

    Implemented as a PWA in localhost (not tested in mobile)

---
+++ WPA_MVC/Controllers/OfflineController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WPA_MVC.Controllers
+{
+    public class OfflineController : Controller
+    {
+        // GET: Offline
+        public ActionResult Index()
+        {
+            return View();
+        }        
+    }
+}
